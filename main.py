7cb3950484eb5df52c7972272d28c48ae1901d51
= <benripka@gmail.com>
2018-10-21 14:59:12 -0600

    made some empty functions

--- main.py
+++ main.py
@@ -39,16 +39,42 @@ def createCar(conn, cno, make, model, year, seats, owner):
 	cur = conn.cursor()
 	cur.execute("insert into cars values(:cno, :make, :model, :year, :seats, :owner)", {"cno" : cno, "make" : make, "model" : model, "year" : year, "seats" : seats, "owner" : owner})
 	
+def createRide(conn, rno, price, rdate, seats, lugDesc, src, dst, driver, cno):
+	cur = conn.cursor()
+	cur.execute("insert into rides values(:rno, :price, :rdate, :seats, :lugDesc, :src, :dst, :driver, :cno)", {"rno":rno, "price":price, "rdate":rdate, "seats":seats, "lugDesc":lugDesc, "src":src, "dst":dst, "driver":driver, "cno":cno})
+
+def offerRide(rdate, numSeats, pricePerSeat, lugDesc, src, dst):
+	pass
+	
+def varifyPwd(psd):
+	# encrypt, decrypt, varify the pwd stored under the members email with the one entered
+	pass
+
+def getUnseenMessages():
+	# On login, display all messages that are marked as unseen 
+	pass
+
+def exitProgram():
+	# Available at all times, ends program.
+	pass
+	
+def searchForRide(src, dst, enroute):
+	#get all (top 5 if to many) rides that match src, dst
+	pass
+	
+def cancelBooking():
+	# Show all bookings the member has offered
+	pass
+	
+	
+
 def main():
 	database = "/home/ben/Desktop/CMPUT291/P1/P1.db"
-	
 	conn = createConnection(database)
 
-	
 	with conn:
 		print("1. Query memebers by email: ")
 		selectMemByEmail(conn, "ben@g.com")
-		
 		selectAllMem(conn)
 		
 if __name__ == '__main__':
