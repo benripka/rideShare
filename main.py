76057396935ceaf1c3a08fea200d36dabb11c953
benripka <benripka@gmail.com>
2018-10-21 18:31:15 -0600

    added the main menu (hasnt been fully implemented) and got the unread messages to display (or they would if the database had any in it)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sqlite3
 from sqlite3 import Error
+from random import randint
 
-global currentUser
+currentUser = None
 
 def createConnection(P1):
 	try:
@@ -49,11 +50,28 @@ def createRide(conn, rno, price, rdate, seats, lugDesc, src, dst, driver, cno):
 	cur = conn.cursor()
 	cur.execute("insert into rides values(:rno, :price, :rdate, :seats, :lugDesc, :src, :dst, :driver, :cno)", {"rno":rno, "price":price, "rdate":rdate, "seats":seats, "lugDesc":lugDesc, "src":src, "dst":dst, "driver":driver, "cno":cno})
 
-def offerRide(rdate, numSeats, pricePerSeat, lugDesc, src, dst):
-	pass
+def offerRide(conn):
+	rdate = input("Please enter the ride date: ")
+	numSeats = input("Number of available seats: ")
+	pricePerSeat = input("Price per seat: ")
+	#TODO: Fill in options
+	lugDesc = input("Bried luggage description (options)") 
+	src = input("locations of departure: ")
+	dst = input("Locations of Arrival: ")
 	
+	cur = conn.cursor()
+	cur.execute("select rno from rides")
+	rno = randint(1, 100000)
+	while rno in cur.fetchall():
+		rno = randint(1, 100000)
+		
+	cur.execute("insert into rides values(:rno, :pricePerSeat, :rdate, :numSeats, :lugDesc, :src, :dst)", {"rno":rno, "pricePerSeat":pricePerSeat, "rdate":rdate, "numSeats":numSeats, "lugDesc":lugDesc, "src":src, "dst":dst})
+	
+def displayOptions(conn):
+	choice = input("What would you like to do next?\nUse the following keys:\nPost a ride (1)\nFind a Ride (2)\nShow My Bookings (3)\nExit (4)\n")
+	return int(choice)
 
-def varifyPwd(conn):
+def login(conn):
 	# encrypt, decrypt, varify the pwd stored under the members email with the one entered
 	email = input("please enter your email: ")
 	pwdTry = input("please enter your password: ")
@@ -62,43 +80,42 @@ def varifyPwd(conn):
 	realPwd = cur.fetchone()
 	if (realPwd[0] == pwdTry):
 		print("Welcome")
+		global currentUser
+		currentUser = email
 		return True
 	else:
 		print("Incorrect Password")
 		return False
 
-def getUnseenMessages():
-	# On login, display all messages that are marked as unseen 
+def displayMessages(conn):
+	email = currentUser
+	cur = conn.cursor()
 	
-	pass
-
-def exitProgram():
-	# Available at all times, ends program.
-	pass
+	cur.execute("select * from inbox where (email=:email and seen = 0)", {"email":email})
+	emails = cur.fetchall()
 	
-def searchForRide(src, dst, enroute):
-	#get all (top 5 if to many) rides that match src, dst
-	pass
-	
-def cancelBooking():
-	# Show all bookings the member has offered
-	pass
+	for email in emails:
+		print(email)
 	
 	
 
 def main():
-	database = "/home/ben/Desktop/CMPUT291/P1/P1.db"
+	database = "/home/ben/Desktop/CMPUT291/rideShare/P1.db"
 	conn = createConnection(database)
 	while True:
 	
 		SorL = input("Enter S to signup or L to login")
 		if SorL == "l":
-			if not varifyPwd(conn):
+			if not login(conn):
 				continue
 			
 		elif SorL == "s":
 			createMem(conn)
 			varifyPwd(conn)
+		
+		displayMessages(conn)
+		choice = displayOptions(conn)
+		switch (choice)
 		
 	
 		
